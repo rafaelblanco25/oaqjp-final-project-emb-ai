95702c5ead4b63faf25737998c63cb05fd2995c0
Rafael Blanco <rblanco25@icloud.com>
2025-02-15 14:53:33 -0400

    Update server.py

--- server.py
+++ server.py
@@ -8,18 +8,20 @@ def render_index_page():
     return render_template('index.html')
 
 @app.route("/emotionDetector")
-def emotion_detector():
+def emotion_detector_route():
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
 
     if response.get("dominant_emotion") is None:
         return "Invalid text! Please try again!"
-    return f"For the given statement, the system response is \
-            'anger': {response.get('anger')}, 'disgust': {response.get('disgust')}, \
-            'fear': {response.get('fear')}, \
-            'joy': {response.get('joy')} and \
-            'sadness': {response.get('sadness')}.\
-            The dominant emotion is {response.get('dominant_emotion')}."
+    
+    anger_response = f"'anger': {response.get('anger')}"
+    disgust_response = f"'disgust': {response.get('disgust')}"
+    fear_response = f"'fear': {response.get('fear')}"
+    joy_response = f"'joy': {response.get('joy')}"
+    sadness_response = f"'sadness': {response.get('sadness')}"
+    dominant_emotion_response = f"The dominant emotion is {response.get('dominant_emotion')}"
+    return f"For the given statement, the system response is {anger_response}, {disgust_response}, {fear_response}, {joy_response} and {sadness_response}. {dominant_emotion_response}."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
