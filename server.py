7f6356ee7de6a953c54cb85bdf75c73c8c6175bd
Rafael Blanco <rblanco25@icloud.com>
2025-02-15 15:02:58 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+""" 
+Server function.
+"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -38,9 +41,10 @@ def emotion_detector_route():
     joy_response = f"'joy': {response.get('joy')}"
     sadness_response = f"'sadness': {response.get('sadness')}"
     dominant_emotion_response = f"The dominant emotion is {response.get('dominant_emotion')}"
-    response_part_1 = f"For the given statement, the system response is {anger_response}, {disgust_response}, {fear_response}, "
-    response_part_2 = f"{joy_response} and {sadness_response}. {dominant_emotion_response}."
-    return f"{response_part_1} {response_part_2}"
+    response_part_1 = f"For the given statement, the system response is {anger_response}, "
+    response_part_2 = f", {disgust_response}, {fear_response}, "
+    response_part_3 = f"{joy_response} and {sadness_response}. {dominant_emotion_response}."
+    return f"{response_part_1} {response_part_2} {response_part_3}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
