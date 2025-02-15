8e076ee4180e436e0dad2bb34680afa549650b38
Rafael Blanco <rblanco25@icloud.com>
2025-02-15 12:22:15 -0400

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,11 +6,5 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
 
     response = requests.post(url, json = myobj, headers=headers)  # Send a POST request to the API with the text and headers
-    print(response.text)  # Print the response text from the API
 
     return response.text  # Return the response text from the API
-
-
-if __name__ == '__main__':
-    text = "I am very happy today"
-    print(emotion_detector(text))
