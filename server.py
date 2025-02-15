a2150712cbc1a40bfe014e27e06aee40b8052ca6
Rafael Blanco <rblanco25@icloud.com>
2025-02-15 14:42:44 -0400

    Update server.py

--- server.py
+++ server.py
@@ -12,18 +12,15 @@ def emotionDetector():
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
 
-    if response.get("dominant_emotion") == None:
+    if response.get("dominant_emotion") is None:
         return "Invalid text! Please try again!"
     
-    return "For the given statement, the system response is 'anger': {}, \
-        'disgust': {}, \
-        'fear': {}, \
-        'joy': {} \
-        and 'sadness': {}. \
-        The dominant emotion is {}."\
-        .format(response.get("anger"), response.get("disgust"), response.get("fear"), \
-                response.get("joy"), response.get("sadness"), response.get("dominant_emotion"))
-
+    return f"For the given statement, the system response is 'anger': {response.get("anger")}, \
+        'disgust': {response.get("disgust")}, \
+        'fear': {response.get("fear")}, \
+        'joy': {response.get("joy")} \
+        and 'sadness': {response.get("sadness")}. \
+        The dominant emotion is {response.get("dominant_emotion")}."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
