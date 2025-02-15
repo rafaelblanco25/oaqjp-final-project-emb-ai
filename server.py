f29c885131b3a6c3de0add5a7dc59d7b860ea593
Rafael Blanco <rblanco25@icloud.com>
2025-02-15 14:04:50 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("EmotionDetection")
+app = Flask("Emotion Detection")
 
 @app.route("/")
 def render_index_page():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 @app.route("/emotionDetector")
 def emotionDetector():
@@ -18,7 +18,8 @@ def emotionDetector():
         'joy': {} \
         and 'sadness': {}. \
         The dominant emotion is {}."\
-        .format(response.get("anger"), response.get("disgust"), response.get("fear"), response.get("joy"), response.get("sadness"), response.get("dominant_emotion"))
+        .format(response.get("anger"), response.get("disgust"), response.get("fear"), \
+                response.get("joy"), response.get("sadness"), response.get("dominant_emotion"))
 
 
 if __name__ == "__main__":
