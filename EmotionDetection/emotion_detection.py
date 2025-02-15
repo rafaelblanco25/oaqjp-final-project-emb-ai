2c28474c9605ef039ca673c2adcac2404b02932c
Rafael Blanco <rblanco25@icloud.com>
2025-02-15 14:15:38 -0400

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,8 +7,13 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
 
     response = requests.post(url, json = myobj, headers=headers)  # Send a POST request to the API with the text and headers
+
     formatted_response = json.loads(response.text)  # Convert the response to JSON
     requiredSetOfEmotions = formatted_response['emotionPredictions'][0]['emotion']  # Extract the emotions from the response
-    requiredSetOfEmotions['dominant_emotion'] = max(requiredSetOfEmotions, key=requiredSetOfEmotions.get)  # Find the dominant emotion
+
+    if response.status_code == 400:
+        requiredSetOfEmotions = { "anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None , "dominant_emotion": None}  # If the response is not successful, set all emotions to 0
+    else:   
+        requiredSetOfEmotions['dominant_emotion'] = max(requiredSetOfEmotions, key=requiredSetOfEmotions.get)  # Find the dominant emotion
 
     return requiredSetOfEmotions  # Return the response text from the API
