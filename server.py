68688b863c05318493c8f2bfe9629a51d43dd371
Rafael Blanco <rblanco25@icloud.com>
2025-02-15 14:49:31 -0400

    Update server.py

--- server.py
+++ server.py
@@ -8,14 +8,18 @@ def render_index_page():
     return render_template('index.html')
 
 @app.route("/emotionDetector")
-def emotionDetector():
+def emotion_detector():
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
 
     if response.get("dominant_emotion") is None:
         return "Invalid text! Please try again!"
-    
-    return (f"For the given statement, the system response is 'anger': {response.get('anger')}, 'disgust': {response.get('disgust')}, 'fear': {response.get('fear')}, 'joy': {response.get('joy')} and 'sadness': {response.get('sadness')}. The dominant emotion is {response.get('dominant_emotion')}.")
+    return f"For the given statement, the system response is \
+            'anger': {response.get('anger')}, 'disgust': {response.get('disgust')}, \
+            'fear': {response.get('fear')}, \
+            'joy': {response.get('joy')} and \
+            'sadness': {response.get('sadness')}.\
+            The dominant emotion is {response.get('dominant_emotion')}."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
